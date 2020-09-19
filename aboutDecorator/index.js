2f9b5c58b33a5984e0d827b9ab1a4470b96e87e5
alan_cao-admin <cao5zy@163.com>
2020-09-19 16:24:36 +0800

    添加babel
    Signed-off-by: alan_cao-admin <cao5zy@163.com>

---
+++ aboutDecorator/index.js
@@ -0,0 +1,54 @@
+//import property from 'lodash/property';
+
+const getUrl = property('url');
+const hasTarget = (args)=>getUrl(args) === 'a';
+const win = window || {};
+const microSvrBaseUrl = 'micro-url';
+
+/*
+function setBaseDomain (target, name, descriptor){
+
+  const originFunc = descriptor.value;
+  descriptor.value = function () {
+
+    const { baseUrl } = win;
+
+    const needSetParam = hasTarget(arguments);
+    if(needSetParam) win.baseUrl = microSvrBaseUrl;
+
+    try
+    {
+      return originFunc.apply(this, arguments);
+    }
+    finally
+    {
+      if(needSetParam) win.baseUrl = baseUrl;
+    }
+  };
+  return descriptor;
+}
+
+class CommonClass {
+  constructor(){
+    console.log('constructed');
+    win.baseUrl = 'http://base-url';
+    this.id = "hello ";
+  }
+  
+  @setBaseDomain
+  getAPIUrl ({url = 'a'}){
+    const fullUrl = win.baseUrl + '/' + url;
+    return fullUrl;
+  }
+}
+
+(()=>{
+
+  const obj = new CommonClass();
+
+  console.log('win.baseUrl begin', win.baseUrl);
+  const url = obj.getAPIUrl();
+  console.log('url', url);
+  console.log('win.baseUrl end', win.baseUrl);
+})();
+*/
